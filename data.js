b29fdd19a351018de8c5ec552c3a6dd134bc47c0
DiversenSato <diversensato@gmail.com>
2022-04-05 23:39:39 +0200

    js data structure

---
+++ data.js
@@ -0,0 +1,25 @@
+var movieData = [
+    {
+        "id": 1,
+        "name": "Saving Private Ryan",
+        "tags": [
+            "war",
+            "drama"
+        ]
+    }
+];
+
+var ratingsData = [
+    {
+        "movieId": 1,
+        "userId": 1,
+        "score": 9
+    }
+];
+
+var userData = [
+    {
+        "id": 1,
+        "username": "Diversensato"
+    }
+];
