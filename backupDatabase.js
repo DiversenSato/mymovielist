019b7985d358fa372909d3b8ed1915bdf64dc2a8
DiversenSato <diversensato@gmail.com>
2022-05-06 15:12:36 +0200

    Database backup script

---
+++ backupDatabase.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const mysql = require('mysql');
+
+const configData = JSON.parse(fs.readFileSync('config.json'));
+const outputFileName = 'databaseBackups/databaseBackup ' + getFormattedDate() + '.tsv';
+
+var dbConnection;
+dbConnection = mysql.createConnection(configData.dbOptions);
+dbConnection.connect((err) => {
+    if (err) throw err;
+
+    dbConnection.query('SELECT * FROM movies;', (err, result, fields) => {
+        if (err) throw err;
+
+        //Write header
+        fs.appendFileSync(outputFileName, 'movieID\tname\trating\tmainActorName\tdirectorName\tgenres\timageURL\n', (err) => {
+            if (err) throw err;
+        });
+
+        //Write data
+        for (let i = 0; i < result.length; i++) { //Loop through every row
+            const r = result[i];
+            let row = r.movieID + '\t' + r.name + '\t' + r.rating + '\t' + r.mainActorName + '\t';
+            row += r.directorName + '\t' + r.genres + '\t' + r.imageURL + '\n';
+
+            fs.appendFileSync(outputFileName, row, (err) => {
+                if (err) throw err;
+            });
+        }
+
+        dbConnection.end();
+    });
+});
+
+function getFormattedDate() {
+    const date = new Date();
+    return date.getFullYear() + '.' + date.getMonth() + '.' + date.getDate() + ' ' + date.getHours() + '_' + date.getMinutes() + '_' + date.getSeconds();
+}
